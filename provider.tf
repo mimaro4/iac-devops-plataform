8787d1377871b1a98717ba7b0011ae4c03154427
mimm <mireia.martinez@gft.com>
2023-12-12 11:53:59 +0100

    modificacion provider

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
  features = {}
- subscription_id = ""
+ subscription_id = "c0680e6c-6ac9-4881-93a6-c87920c77c9b"
  client_id	 = ""
  client_secret	 = ""
- tenant_id	 = ""
+ tenant_id	 = "b91b4c46-c3c5-4a82-be1c-6009b300244"
 {
